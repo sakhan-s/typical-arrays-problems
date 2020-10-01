eb135cefcc105da39e17f342d3cdb01a4d0879b0
sakhan-s <60611884+sakhan-s@users.noreply.github.com>
2020-10-01 21:56:19 +0300

    feat: fixed solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-
-exports.min = function min (array) {
-  return Math.min.apply(null, array);
+exports.min = function min(array) {
+    return (array && array.length) ? Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
-  return Math.max.apply(null, array);;
+
+exports.max = function max(array) {
+    return (array && array.length) ? Math.max(...array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return array.reduce((a, b) => (a + b)) / array.length;
+exports.avg = function avg(array) {
+    return (array && array.length) ? ((array.reduce((acc, cur) => acc + cur)) / array.length) : 0;
 }
